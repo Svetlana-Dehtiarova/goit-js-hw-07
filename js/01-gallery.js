856c6cf842873e8d38afd1a6df58d661f8fba145
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2022-12-26 09:33:52 +0200

    Update Task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryItems(items) {
+  return items.map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(``);
+}
+console.log(createGalleryItems(galleryItems));
+
+const galleryConteiner = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('afterbegin', galleryMarkup)
+
+galleryConteiner.addEventListener(`click`, onGalleryConteinerClick)
+
+function onGalleryConteinerClick(event) {
+    event.preventDefault();
+    if (event.target.closest('.gallery__link')) {
+        return;
+    }
+    const instance = basicLightbox.create(
+            `<img src="${event.target.closest('img').dataset.source}" width="800" height="600">`);
+  instance.show();
+
+  const onKeydownEsc = (event) => {
+    console.log(event.code);
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  };
+
+  window.addEventListener("keydown", onKeydownEsc);
+}
